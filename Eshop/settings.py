bb8d5df8c3cc5f494e92aeb11e579594b640184f
dhamanp10 <prateekdhaman10@gmail.com>
2021-05-23 02:46:06 +0530

    done

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "/store/templates/"
 MEDIA_URL = "/images/download/"
 MEDIA_ROOT = BASE_DIR
 
